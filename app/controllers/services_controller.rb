c12fe4c15e0617f6803cfa4b328fbe90326e8b69
Edward Fernando <edward.fer@gmail.com>
2014-05-09 13:25:51 +0700

    Add service-crud

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,26 @@
 class ServicesController < ApplicationController
 
+	def index
+		@services = Service.all
+	end
+
 	def new
 	end
 
 	def create
-		
+		@service = Service.new(service_param)
+
+		@service.save
+		redirect_to @service
+	end
+
+	def show
+		@service = Service.find(params[:id])
+	end
+
+	private
+	def service_param
+		params.require(:services).permit(:name, :description)
 	end
 
 end
