bec868749e39431711bdf33a4b88948d28fa006c
Edward Fernando <edward.fer@gmail.com>
2014-05-24 22:42:17 +0700

    Add current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def today
     DateTime.now.strftime("%F")
   end
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
