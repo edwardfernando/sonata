7ae0ff99d39efdc7250f3a42dfbcae0d0ae376ff
Edward Fernando <edward.fer@gmail.com>
2014-06-10 18:03:03 +0700

    Change logic sequence how to save/update

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -25,21 +25,29 @@ class ServicesController < ApplicationController
 
 	def create
 		@service = Service.new(service_param)
-
 		authorize @service
 
 		role_array = params[:service][:role] ||= []
 		people_array = params[:service][:person] ||= []
 
-		role_array.each_with_index do |role,index|
-			Schedule.create(service:@service, role:Role.find(role), person:Person.find(people_array[index]))
-		end
+		role_array.delete_if(&:empty?)
+		people_array.delete_if(&:empty?)
 
-		if @service.save
-			redirect_to services_path + "?date=" + @service.date.strftime("%F")
-		else
-			@service.errors.add(:schedules) if role_array.empty?
+		if !@service.valid?
 			render "new"
+		elsif !role_array.any? || !people_array.any?
+			@service.errors.add(:schedules)
+			render "new"
+		else
+			@service.save(service_param)
+			# Service.find(params[:id]).schedules.destroy_all
+
+			role_array.each_with_index do |role,index|
+				person = Person.find(people_array[index])
+				schedule = Schedule.create(service:@service, role:Role.find(role), person:person)
+			end
+
+			redirect_to services_path + "?date=" + @service.date.strftime("%F")
 		end
 
 	end
@@ -60,17 +68,26 @@ class ServicesController < ApplicationController
 		role_array = params[:service][:role] ||= []
 		people_array = params[:service][:person] ||= []
 
-		if @service.update(service_param) && !role_array.empty?
+		role_array.delete_if(&:empty?)
+		people_array.delete_if(&:empty?)
+
+
+		if !@service.update(service_param)
+			render "edit"
+		elsif !role_array.any? || !people_array.any?
+			@service.errors.add(:schedules)
+			render "edit"
+		else
+
+			@service.update(service_param)
 			Service.find(params[:id]).schedules.destroy_all
 
 			role_array.each_with_index do |role,index|
-				Schedule.create(service:@service, role:Role.find(role), person:Person.find(people_array[index]))
+				person = Person.find(people_array[index])
+				schedule = Schedule.create(service:@service, role:Role.find(role), person:person)
 			end
 
 			redirect_to services_path + "?date=" + @service.date.strftime("%F")
-		else
-			@service.errors.add(:schedules) if role_array.empty?
-			render "edit"
 		end
 
 	end
