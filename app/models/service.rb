a95f5784afc717c9443fc2fcb9a21e18a8315036
Edward Fernando <edward.fer@gmail.com>
2014-06-10 17:58:28 +0700

    Use public_activity method (tracked) to Service & Schedule

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,14 @@
 class Service < ActiveRecord::Base
+
+    include PublicActivity::Model
+    tracked except: [:update], owner: Proc.new{ |controller, model| controller.current_person },
+      :params => {
+        :model_details => proc {|controller, model_instance| model_instance.to_json}
+      }
+
     has_many :schedules, :dependent => :destroy, inverse_of: :service
 
     validates :name, presence: true
     validates :date, presence: true
+
 end
