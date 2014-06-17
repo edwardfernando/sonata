dcff6f71f61764a2e6257f8aac416a2ae2b31c7b
Edward Fernando <edward.fer@gmail.com>
2014-06-17 22:52:00 +0700

    Remove parse_avatar ~ moved to people_helper

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,11 +1,2 @@
 module ProfilesHelper
-
-  def parse_avatar  
-    if current_person.provider == "facebook"
-      current_person.avatar_url + "?type=large"
-    elsif current_person.provider == "twitter"
-      current_person.avatar_url.gsub "_normal", ""
-    end
-  end
-
 end
