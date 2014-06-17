727dab0f91b2c5b1158504208a94825ca611eb47
Edward Fernando <edward.fer@gmail.com>
2014-06-17 22:51:20 +0700

    Add parser_avatar based on person

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,11 @@
 module PeopleHelper
+
+  def parse_avatar (person)
+    if person.provider == "facebook"
+      person.avatar_url + "?type=large"
+    elsif person.provider == "twitter"
+      person.avatar_url.gsub "_normal", ""
+    end
+  end
+
 end
