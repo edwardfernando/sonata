b745ee9373038ec866c151b9bbca9786e7dd494b
Edward Fernando <edward.fer@gmail.com>
2014-06-18 23:31:54 +0700

    Get unconfirmed schedules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def get_unconfirmed_schedules
+    counter = 0
+    current_person.schedules.each do |schedule|
+      if schedule.confirmed_at.nil?
+        counter = counter + 1
+      end
+    end
+    return counter
+  end
+
 end
