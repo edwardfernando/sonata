fad5d4d4947043261626b18a7744fcf7a00e4173
Edward Fernando <edward.fer@gmail.com>
2014-06-18 08:00:58 +0700

    Necessary file for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Sonata::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mandrillapp.com",
+    :port => 587,
+    :domain => "sonata.com",
+    :user_name => "edward.fer@gmail.com",
+    :password => "M3gO3RuXdItIF7q_pLkABA"
+  }
 end
