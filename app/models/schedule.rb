c00bd499dcad3289bd2a002fa9045a585bc87f07
Edward Fernando <edward.fer@gmail.com>
2014-07-27 22:15:15 +0700

    Add role name and servie name as activity param

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,11 @@
 class Schedule < ActiveRecord::Base
   include PublicActivity::Model
+  tracked except: [:update, :destroy, :create], owner: Proc.new{ |controller, model| controller.current_person },
+    :params => {
+      :model_details => proc {|controller, model_instance| model_instance.to_json},
+      :role_name => proc {|controller, model_instance| model_instance.role.name},
+      :service_name => proc {|controller, model_instance| model_instance.service.name}
+    }
 
   belongs_to :service
   belongs_to :person
