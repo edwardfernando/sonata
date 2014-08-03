e567a22f3abb3edbb73fdc6cb08d74d91f43165f
Edward Fernando <edward.fer@gmail.com>
2014-08-03 23:54:15 +0700

    no need to use separated config - use figaro instead

--- config/initializers/configs.rb
+++ config/initializers/configs.rb
@@ -1,3 +1,3 @@
-FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
-TWITTER_CONFIG = YAML.load_file("#{::Rails.root}/config/twitter.yml")[::Rails.env]
-MANDRILAPP_CONFIG = YAML.load_file("#{::Rails.root}/config/mandrilapp.yml")[::Rails.env]
+# MANDRILAPP_CONFIG = YAML.load_file("#{::Rails.root}/config/mandrilapp.yml")[::Rails.env]
+# FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+# TWITTER_CONFIG = YAML.load_file("#{::Rails.root}/config/twitter.yml")[::Rails.env]
