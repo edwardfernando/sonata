27511661137ba3689d5c4aff189f0850e56e2fa7
Edward Fernando <edward.fer@gmail.com>
2014-08-12 22:52:09 +0700

    Add test schedule to test the logs path are correct

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,11 @@
 set :environment, "development"
-set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+set :output, {:error => "/home/deploy/sonata/logs/cron/error_log.log", :standard => "/home/deploy/sonata/logs/cron/cron_log.log"}
 
 every 1.day, :at => "10:00 am" do
   rake "reminder:schedule"
   rake "reminder:not_confirmed"
+end
+
+every 1.minutes do
+  rake "reminder:puts_test"
 end
