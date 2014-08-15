09d80080975881909ac873a404b27599e9341773
Edward Fernando <edward.fer@gmail.com>
2014-08-15 17:58:13 +0700

    update environment

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :environment, "development"
+set :environment, "production"
 set :output, {:error => "/home/deploy/sonata/logs/cron/error_log.log", :standard => "/home/deploy/sonata/logs/cron/cron_log.log"}
 
 every 1.day, :at => "10:00 am" do
