b69af3d2ba2eaf74a85975b7c73f953bc2b85970
Edward Fernando <edward.fer@gmail.com>
2014-10-27 19:30:53 +0700

    helper method for devise modified login page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,15 @@
 module WelcomeHelper
+
+  def resource_name
+    :person
+  end
+
+  def resource
+    @resource ||= Person.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:person]
+  end
+
 end
