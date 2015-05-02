e842aff66a3810d9288fdf347eae26b5ea6e4261
Edward Fernando <edward.fer@gmail.com>
2015-05-02 18:54:16 +0700

    add category coloum to service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407115746) do
+ActiveRecord::Schema.define(version: 20150502075843) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20150407115746) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "created_by"
+    t.integer  "category"
   end
 
   create_table "skillsets", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,18 +16,3 @@ Role.create(name: 'WL', created_at: DateTime.now, updated_at: DateTime.now)
 Role.create(name: 'Keyboardis', created_at: DateTime.now, updated_at: DateTime.now)
 Role.create(name: 'AV', created_at: DateTime.now, updated_at: DateTime.now)
 Role.create(name: 'Pianis Ibadah', created_at: DateTime.now, updated_at: DateTime.now)
-
-Person.delete_all
-Person.create(id: '1',
-    email: 'edward.fer@gmail.com',
-    name: 'Edward Fernando/Admin',
-    provider: 'facebook',
-    uid: '733980098',
-    oauth_token: 'CAACkhqIY8uUBAMBt1AZAXJ5iaGrCFAxYo8PFvUZC3KMdKiLQZBaJ03N6fIx7h0cpLWd7hFAWgIxSFebvPd6e3x1siroqiYXMjkRs1DgvPiCMEim5MUNRVZCJNhXmbsozUMg6HUtxdakkkKktfIoXVk6qJYTSZBZAwfQJZCZCiFXt9VeXh58b0JUn17pU61UJRhpqGvXgLNZALV1N9h2azCRgZC',
-    oauth_expires_at: '2014-09-22 13:37:11.000000',
-    avatar_url: 'http://graph.facebook.com/733980098/picture',
-    random_id: 'exSh-',
-    phone_number_1: '0817211431',
-    is_approved: 1,
-    approved_date: '2001-01-01 07:00:00',
-    role: 2)
